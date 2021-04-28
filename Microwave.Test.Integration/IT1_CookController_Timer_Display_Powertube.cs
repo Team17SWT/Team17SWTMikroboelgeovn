e0150eba475583eaba73e4cd1baabf5045ae1908
augusthjerrild <augusthjerrild@gmail.com>
2021-04-28 08:32:28 +0200

    made private uut

--- Microwave.Test.Integration/IT1_CookController_Timer_Display_Powertube.cs
+++ Microwave.Test.Integration/IT1_CookController_Timer_Display_Powertube.cs
@@ -1,12 +1,16 @@
+using Microwave.Classes.Controllers;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
     public class Tests
     {
+        private CookController uut;
+
         [SetUp]
         public void Setup()
         {
+          
         }
 
         [Test]
